e3b5910ceb1847bea0ea314fe00aa5f4cb9704d2
Allen Canning <canning@Perseus.local>
2017-03-13 10:12:47 -0400

    Removed unrequired libraries

--- print_rpi_table.py
+++ print_rpi_table.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python
 
-import time
-from datetime import datetime
 import boto.dynamodb2
 from boto.dynamodb2.fields import HashKey, RangeKey, KeysOnlyIndex, GlobalAllIndex
 from boto.dynamodb2.table import Table
 from boto.dynamodb2.types import NUMBER
-import pprint
-
-pp = pprint.PrettyPrinter(indent=4)
 
 ## Query DB and get all the teams
 def getTeams():
@@ -49,20 +44,22 @@ def getRecords(games):
 
   return record
 
-games = getGames()
+def printRpiTableHandler (event, context):
 
-record = getRecords(games)
+  games = getGames()
 
-teams = getTeams()
+  record = getRecords(games)
 
-print "<table>\n<tr><th>Name</th><th>Wins</th><th>Loss</th><th>WPCT</th></tr>\n"
-for team in teams:
-  print "<tr><td>"+team['name']+"</td>"
-  print "<td>"+str(record[team['name']]['win'])+"</td>"
-  print "<td>"+str(record[team['name']]['loss'])+"</td>"
-  winpct = record[team['name']]['win']/(record[team['name']]['win']+record[team['name']]['loss'])
-  print "<td>%0.2f" % winpct
-  print "</td>"
-  print "</tr>\n"
+  teams = getTeams()
 
-print "</table>\n"
+  print "<table>\n<tr><th>Name</th><th>Wins</th><th>Loss</th><th>WPCT</th></tr>\n"
+  for team in teams:
+    print "<tr><td>"+team['name']+"</td>"
+    print "<td>"+str(record[team['name']]['win'])+"</td>"
+    print "<td>"+str(record[team['name']]['loss'])+"</td>"
+    winpct = record[team['name']]['win']/(record[team['name']]['win']+record[team['name']]['loss'])
+    print "<td>%0.2f" % winpct
+    print "</td>"
+    print "</tr>\n"
+
+  print "</table>\n"
