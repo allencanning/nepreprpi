4a1328db4249ac3a370e56211db607544ac0c243
allendcanning <canning@canningbranch.com>
2017-03-13 18:32:30 -0400

    Finished functional coding of print_rpi_table.py

--- print_rpi_table.py
+++ print_rpi_table.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from __future__ import division
 import json
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
@@ -25,7 +26,7 @@ def getGames():
   return games['Items']
 
 ## Loop through the games and calculate the wins and losses
-def getRecords(games):
+def getRecords(games,teams):
   record = {}
   for game in games:
     # Set the wins and losses to 0
@@ -41,15 +42,43 @@ def getRecords(games):
     record[game['winner']]['win'] += 1
     record[game['loser']]['loss'] += 1
 
+  # calculate winpct
+  for team in teams:
+    record[team['name']]['winpct'] = record[team['name']]['win']/(record[team['name']]['win']+record[team['name']]['loss'])
+
   return record
 
-def printRpiTableHandler (event, context):
+def getOwpct(games,record,team):
+  numteams = 0
+  owpct = 0
+  for g in games:
+    if team != g['winner']:
+      numteams += 1
+      owpct += record[g['winner']]['winpct']
+    if team != g['loser']:
+      numteams += 1
+      owpct += record[g['loser']]['winpct']
 
-  games = getGames()
+  owpctavg = owpct / numteams
+  
+  return owpctavg
 
-  record = getRecords(games)
+def getOOwpct(games,record,team):
+  numteams = 0
+  oowpct = 0
+  for g in games:
+    if team != g['winner']:
+      numteams += 1
+      owpct = getOwpct(games,record,g['winner'])
+      oowpct += owpct
+    if team != g['loser']:
+      numteams += 1
+      owpct = getOwpct(games,record,g['loser'])
+      oowpct += owpct
 
-  teams = getTeams()
+  oowpctavg = oowpct / numteams
+
+  return oowpctavg
 
 def printRpiTableHandler (event, context):
 
@@ -60,13 +89,18 @@ def printRpiTableHandler (event, context):
   teams = getTeams()
 
   content = "<html><body>\n"
-  content += "<table>\n<tr><th>Name</th><th>Wins</th><th>Loss</th><th>WPCT</th></tr>\n"
+  content += "<table>\n<tr><th>Name</th><th>Wins</th><th>Loss</th><th>WPCT</th><th>OWPCT</th></tr>\n"
   for team in teams:
     content += "<tr><td>"+team['name']+"</td>"
     content += "<td>"+str(record[team['name']]['win'])+"</td>"
     content += "<td>"+str(record[team['name']]['loss'])+"</td>"
-    winpct = record[team['name']]['win']/(record[team['name']]['win']+record[team['name']]['loss'])
-    content += "<td>%0.2f" % winpct
+    content += "<td>%0.2f" % record[team['name']]['winpct']
+    content += "</td>"
+    owpct = getOwpct(games,record,team['name'])
+    content += "<td>%0.2f" % owpct
+    content += "</td>"
+    oowpct = getOOwpct(games,record,team)
+    content += "<td>%0.2f" % oowpct
     content += "</td>"
     content += "</tr>\n"
 
