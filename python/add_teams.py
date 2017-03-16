7988895c9421eca842ae30444d6c2aa567d78c65
allendcanning <canning@canningbranch.com>
2017-03-16 13:00:53 -0400

    Modified add_teams to take command line options and added data file

--- python/add_teams.py
+++ python/add_teams.py
@@ -5,33 +5,48 @@ import boto.dynamodb2
 from boto.dynamodb2.fields import HashKey, RangeKey, KeysOnlyIndex, GlobalAllIndex
 from boto.dynamodb2.table import Table
 from boto.dynamodb2.types import NUMBER
+from optparse import OptionParser
 
-nepreprpiteams = Table('nepreprpiteams')
+def readRecords(infile):
+  records = []
+  with open(infile,"r") as f:
+    for line in f:
+      # split the line into fields
+      data = line.split(",")
+      # Build array of data records
+      records.append({'name': data[0], 'season': int(data[1]), 'url': data[2]})
 
-nepreprpiteams.put_item(data={
-  'name': 'Brooks',
-  'season': 2017,
-  'url': 'http://www.brooksschool.org',
-})
+  return records
 
-nepreprpiteams.put_item(data={
-  'name': 'Middlesex',
-  'season': 2017,
-  'url': 'http://www.mxschool.edu',
-})
-nepreprpiteams.put_item(data={
-  'name': 'Deerfield',
-  'season': 2017,
-  'url': 'http://www.deerfield.edu',
-})
-nepreprpiteams.put_item(data={
-  'name': 'Andover',
-  'season': 2017,
-  'url': 'http://www.andover.edu',
-})
-nepreprpiteams.put_item(data={
-  'name': 'Pingree',
-  'season': 2017,
-  'url': 'http://www.pingree.org',
-})
+def writeRecords(table,records):
+  for record in records:
+    print "Adding team "+record['name']
+    table.put_item(data=record)
 
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="file",
+                  help="Team import file")
+parser.add_option("-t", "--table", dest="table",
+                  help="Dynamodb Table to use (defaults to nepreprpiteams")
+
+(options, args) = parser.parse_args()
+
+if not options.file:
+  print "Please specify an input file\n\n"
+  parser.print_help()
+  exit(1)
+
+# Set the table from the options listed on the command line
+if not options.table:
+  table = "nepreprpiteams"
+else:
+  table = options.table
+
+# connect to the dynamodb table
+t = Table(table)
+
+# Read the file and build records
+records = readRecords(options.file)
+
+# Write the records to the DB
+writeRecords(t,records)
